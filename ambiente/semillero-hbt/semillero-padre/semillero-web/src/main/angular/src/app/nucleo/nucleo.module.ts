887983c12982d9ef19c49205c627dd78e456fe18
Cristian Ramirez <cdrz01@gmail.com>
2019-04-23 16:14:14 -0500

    se importan los componentes en el nucleo.ts

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/nucleo/nucleo.module.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/nucleo/nucleo.module.ts
@@ -5,9 +5,12 @@ import { EncabezadoComponent } from './capa/encabezado/encabezado.component';
 import { MainComponent } from './capa/main/main.component';
 import { PiepaginaComponent } from './capa/piepagina/piepagina.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { ApoyoComponent } from './capa/main/apoyo/apoyo.component';
+import { ModeloComponent } from './capa/main/apoyo/modelo/modelo.component';
+import { CreacionClienteComponent } from './capa/main/creacion-cliente/creacion-cliente.component';
 
 @NgModule({
-  declarations: [CapaComponent, EncabezadoComponent, MainComponent, PiepaginaComponent, ServiciosComponent],
+  declarations: [CapaComponent, EncabezadoComponent, MainComponent, PiepaginaComponent, ServiciosComponent, ApoyoComponent, ModeloComponent, CreacionClienteComponent],
   imports: [
     CommonModule
   ]
