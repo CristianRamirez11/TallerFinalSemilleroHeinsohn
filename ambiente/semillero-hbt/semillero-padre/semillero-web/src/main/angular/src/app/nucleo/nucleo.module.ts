7983deeeaf83799e17f4e03b94dd8d2e266d5a46
Cristian Ramirez <cdrz01@gmail.com>
2019-04-23 15:28:53 -0500

    se crean los componentes

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/nucleo/nucleo.module.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/nucleo/nucleo.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CapaComponent } from './capa/capa.component';
+import { EncabezadoComponent } from './capa/encabezado/encabezado.component';
+import { MainComponent } from './capa/main/main.component';
+import { PiepaginaComponent } from './capa/piepagina/piepagina.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CapaComponent, EncabezadoComponent, MainComponent, PiepaginaComponent],
   imports: [
     CommonModule
   ]
