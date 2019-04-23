a3a6f64f57c0541125e84419c538ba9eef2bb280
Cristian Ramirez <cdrz01@gmail.com>
2019-04-23 15:19:59 -0500

    se crea el modulo nucleo

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app.component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  title = 'Aplicacion para Gestion de pedidos de restaurante';
 }
